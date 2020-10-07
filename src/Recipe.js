78973b206e62f7291a6223948efb83332e8b71ed
Jae Young Choi <jaeyoungnsb@gmail.com>
2020-10-06 22:25:34 -0400

    added ingredients

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={title}>{ingredient.text}</li>
+        ))}
+      </ul>
       <p>{calories}</p>
       <img src={image} alt="" />
     </div>
