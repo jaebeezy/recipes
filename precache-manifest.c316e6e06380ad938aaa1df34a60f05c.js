23905ae8a1c6d15f1680a9ce1d836320a0553e3e
Jae Young Choi <jaeyoung@Jaes-Mac-mini.home>
2020-12-09 19:02:54 -0500

    Updates

---
+++ precache-manifest.c316e6e06380ad938aaa1df34a60f05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899ee040c3b33b3a40eb0e892e2a0714",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "ac955a05064bd37ca69b",
+    "url": "/recipes/static/css/main.e8ed3468.chunk.css"
+  },
+  {
+    "revision": "9aee968d8d20d6448895",
+    "url": "/recipes/static/js/2.b43194c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.b43194c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac955a05064bd37ca69b",
+    "url": "/recipes/static/js/main.19cdf77c.chunk.js"
+  },
+  {
+    "revision": "65aed4a3aaa500d440aa",
+    "url": "/recipes/static/js/runtime-main.9fddb04f.js"
+  }
+]);
